93b42c598802d2e3ba228915a5a12936914ea1e2
Hunter Stevens <onebree@gmail.com>
2016-11-08 14:44:42 -0500

    Take into account two kinds of saved items

--- saved.rb
+++ saved.rb
@@ -14,13 +14,21 @@ Dotenv.load
 
 @paginated_results = []
 
+def raw_json(string)
+  return if string.nil?
+
+  string.gsub("&lt;", "<")
+        .gsub("&gt;", ">")
+        .gsub("&amp;", "&")
+end
+
 def get_saved
   options = { :category => "saved", :limit => 100 }
   options[:after] = @after_id if @after_id
 
   results = @client.my_content(options).results
 
-  return nil if results.empty?
+  return if results.empty?
   
   @paginated_results.push results
 
@@ -38,10 +46,24 @@ end
 saved_items = @paginated_results.flatten
 
 CSV.open("saved_items.csv", "w") do |csv|
-  csv << ["name", "title", "subreddit", "permalink", "url"]
+  csv << [
+    "name", "kind", "id", "subreddit",            # Comments and links
+    "link_id", "body", "link_title", "link_url",  # Comments only
+    "is_self", "permalink", "url", "title"        # Links only
+  ]
 
   saved_items.each do |item|
-    values = item.attributes.values_at(:name, :title, :subreddit, :permalink, :url)
+    item.attributes[:body]       = raw_json(item.attributes[:body])
+    item.attributes[:link_title] = raw_json(item.attributes[:link_title])
+    item.attributes[:link_url]   = raw_json(item.attributes[:link_url])
+    item.attributes[:url]        = raw_json(item.attributes[:url])
+    item.attributes[:title]      = raw_json(item.attributes[:title])
+
+    values = item.attributes.values_at(
+      :name, :kind, :id, :subreddit,
+      :link_id, :body, :link_title, :link_url,
+      :is_self, :permalink, :url, :title
+    )
 
     csv << values
   end
